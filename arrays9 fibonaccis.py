95c56cf37b069092253288fc25ed8e0f32211d96
Andres Guerra <andresfg13789@gmail.com>
2023-06-12 16:06:36 -0500

    correciones arrays 1

--- arrays9 fibonaccis.py
+++ arrays9 fibonaccis.py
@@ -1,21 +1,24 @@
-cantidad = int(input("Cantidad de numeros"))
-Fibonaccis = []
-contador = 1
-while contador <= cantidad:
-    num = int(input("Ingrese un numero"))
+cantidad = int(input("Cantidad de números: "))
+fibonaccis = []
+
+for _ in range(cantidad):
+    num = int(input("Ingrese un número: "))
     num1 = 0
     num2 = 1
     secuencia = 0
-    while secuencia <= num:
+
+    while secuencia < num:
         secuencia = num1 + num2
         num1 = num2
         num2 = secuencia
+
         if secuencia == num:
-            Fibonaccis.append(num)
-    contador += 1
+            fibonaccis.append(num)
+            break
 
-Fibonaccis = sorted(Fibonaccis)
-for i in Fibonaccis:
-    print("Los fibonaccis encontrads son:", Fibonaccis)
+fibonaccis = sorted(fibonaccis)
 
-print("El fibonacci menor es: ", Fibonaccis[0], "Y el fibonacci mayor es: ", Fibonaccis[-1])
+print("Los números de Fibonacci encontrados son:", fibonaccis)
+print("El número de Fibonacci más pequeño es:", fibonaccis[0])
+print("El número de Fibonacci más grande es:", fibonaccis[-1])
+
