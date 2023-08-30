ed59da3115c58da42374d6b63d729293403a0d5d
Andres Guerra <andresfg13789@gmail.com>
2023-08-30 09:51:26 -0500

    con ejercicio sustentacion

--- Entrega del taller.py
+++ Entrega del taller.py
@@ -17,7 +17,8 @@ def menu_inicio():
             ║ 3. Taller 3                            ║
             ║ 4. Evaluación 1                        ║
             ║ 5. Evaluación 2                        ║
-            ║ 6. Salir                               ║
+            ║ 6. Ejercicio sustentacion              ║
+            ║ 7. Salir                               ║
             ║                                        ║
             ╚════════════════════════════════════════╝
         """ + Fore.RESET)
@@ -3707,6 +3708,57 @@ def menu_inicio():
                 input("Presione Enter para continuar...")
                 os.system("cls" if os.name == "nt" else "clear")
         if respuesta == 6:
+            def ejercicio_sustentacion():
+                #Hallar el primo mayor de las filas impares de la matriz
+                import random
+
+                def es_primo(numero):
+                    condicion = 1
+                    divisores = 0
+                    while condicion <= numero:
+                        if numero % condicion == 0:
+                            divisores += 1
+                        condicion += 1
+                    if divisores == 2:
+                        return True
+                    return False
+
+                def hacer_matriz(matriz):
+                    dimensiones = random.randint(4,6)
+                    for fila in range(dimensiones):
+                        fila_actual = []
+                        for columna in range(dimensiones):
+                            fila_actual.append(random.randint(1,15))
+                        matriz.append(fila_actual)
+
+                def mostrar_matriz(matriz):
+                    for fila in matriz:
+                        print(" ".join("{:4}".format(n) for n in fila))
+
+                def mostrar_primos_filas_impares(matriz):
+                    primos_filas_impares = []
+                    for fila in range(len(matriz)):
+                        if fila % 2 == 0:
+                            primo_mayor = 0
+                            for columna in range(len(matriz)):
+                                numero = matriz[fila][columna]
+                                if es_primo(numero):
+                                    if numero > primo_mayor:
+                                        primo_mayor = numero
+                            primos_filas_impares.append(primo_mayor)
+                    print(primos_filas_impares)
+
+                matriz = []
+                hacer_matriz(matriz)
+                print("\nMatriz Original")
+                mostrar_matriz(matriz)
+                print("\nMatriz con primo mayor de las filas impares")
+                mostrar_matriz(matriz)
+                mostrar_primos_filas_impares(matriz)
+            ejercicio_sustentacion()
+            input("Presione Enter para continuar...")
+            os.system("cls" if os.name == "nt" else "clear")
+        if respuesta == 7:
             print(Fore.LIGHTCYAN_EX + """
             ╔════════════════════════════════════════╗
             ║                                        ║
